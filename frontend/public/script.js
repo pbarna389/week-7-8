9de6a45f1c269d312b69ae4ec7183bbc334078bd
pbarna389 <64441402+pbarna389@users.noreply.github.com>
2022-05-09 13:06:52 +0200

    Session end

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,8 +1,10 @@
+console.log(varVariable);
 let globalVar1 = 12;
 
 let globalVar2 = () => {
     console.log(`runs from the inside`);
     let functionVar1 = "Hello";
+
 
     let functionVar2 = () => {
         console.log(`'Sup`);
@@ -21,13 +23,63 @@ let globalVar2 = () => {
     functionVar2();
 
     // console.log(functionVar1, globalVar1);
+    if (functionVar1 === "Hello") {
+        var blockVar1 = "Mayor Tom reporting in";
+    } else {
+        // console.log(blockVar1);
+    };
+
+    console.log(blockVar1);
+
 };
 
 globalVar2();
 
-const loadEvent = () => {
-    const rootEl = document.querySelector('#root');
+var varVariable = 1;
 
+let letVariable1;
+console.log(letVariable1);
+
+let letVariable2 = 2;
+console.log(letVariable2);
+
+const constVariable1 = "ciao";
+// constVariable1 = "hello";
+console.log(constVariable1);
+
+const obj1 = {};
+obj1.key = "value"
+// obj1 = { key: "value" };
+
+console.log(obj1);
+
+const globalVar4 = "apple2";
+
+const globalVar5 = () => {
+    return "pear"
+};
+
+const globalVar6 = () => {
+    return "banana"
+};
+
+const globalVar3 = (parameter1, parameter2, parameter3) => {
+    console.log(parameter1);
+    console.log(parameter2);
+    console.log(parameter3());
+};
+
+globalVar3(globalVar5(), globalVar4, globalVar6);
+
+const loadEvent = (param1) => {
+    const rootEl = document.querySelector('#root');
+    console.log(rootEl);
+    // console.log(param1)
+
+    rootEl.addEventListener('click', function (event) {
+        console.log(event.currentTarget);
+        event.currentTarget.insertAdjacentHTML('beforeend', "Clicked,")
+    });
 };
 
 window.addEventListener('load', loadEvent);
